da040305d4d46010b5b22a6aca11ee9d0d148f2c
Steven Baron <Steven.Baron@strongmind.com>
2021-02-09 14:25:01 -0700

    added object shape

--- index.mjs
+++ index.mjs
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+const purchaseObj = (title, price, inStock) => ({title, price, inStock});
+
 const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021';
 
 axios.get(url, {responseType: 'text'})
      .then(({data}) => cheerio.load(data))
-     .then($ => $('.speech-info')[0].text())
+     .then($ => $('.items-grid-view').first().text())
      .then(console.log)
+     
 
