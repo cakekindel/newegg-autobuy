aecd08c6315fad2887cf0331bea21d2ae2b64189
Orion Kindel <cakekindel@gmail.com>
2021-02-09 15:57:42 -0700

    add pushbullet support

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const {timer} = require('rxjs');
 const RxOp = require('rxjs/operators/index.js');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const PushBullet = require('pushbullet');
 
-const purchaseObj = (title, price, url, inStock) => ({title, price, url, inStock});
-
-const sendNotification = (data) => {
-     let filteredList = data.filter(e => e.inStock);
-     console.log(filteredList);
-}
-
+// helpers
+const throwE = e => {throw e;};
+const envOrThrow = key => process.env[key] || throwE(new Error(`env var ${key} not set`));
 const isEmpty = len => len.length === 0;
 const not = f => (...args) => !f(...args);
+
+/**
+ * construct an "item" object.
+ * This is the common representation for products across sources.
+ */
+const purchaseObj = (title, price, url, inStock) => ({title, price, url, inStock});
+
+const sendNotification = item => {
+  const pb = new PushBullet(envOrThrow('PB_API_KEY'));
+  const notify = device => pb.link(device.iden, 'Newegg Alert', item.url, item.title);
+
+  pb.devices()
+    .then(({devices}) => devices.filter(d => d.icon === 'phone' && d.active))
+    .then(devices     => devices.forEach(notify))
+}
+
+// newegg stuff
 const isAd = $ => (_, el) => $('.txt-ads-box', el).length > 0 ? true : false;
 const parseItem = $ => (_, el) => purchaseObj(
                                     $(".item-title", el).text(),
@@ -21,19 +35,22 @@ const parseItem = $ => (_, el) => purchaseObj(
                                   );
 
 const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021&PageSize=96';
+const getNeweggItems = axios.get(url, {responseType: 'text'})
+                            .then(({data}) => cheerio.load(data))
+                            .then($ => $('.items-grid-view .item-cell')
+                                         .filter(not(isAd($)))
+                                         .map(parseItem($))
+                                         .get()
+                            );
 
-const getInStock = () => axios.get(url, {responseType: 'text'})
-                              .then(({data}) => cheerio.load(data))
-                              .then($ => $('.items-grid-view .item-cell')
-                                           .filter(not(isAd($)))
-                                           .map(parseItem($))
-                                           .get()
-                              )
-                              .then(arr => arr.filter(item => item.inStock))
+// aggregate sources, filter out of stock
+const getInStock = () => getNeweggItems()
+                           .then(arr => arr.filter(item => item.inStock));
 
 timer(0, 500)
   .pipe(
-    RxOp.flatMap(getInStock),
+    // RxOp.flatMap(getInStock),
+    RxOp.map(() => [{title: 'Testing 123', url: 'https://www.cheese.com'}]),
     RxOp.tap(data => {
       if (data.length > 0) {
         data.forEach(sendNotification)
