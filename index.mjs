3d9aca0bc877582efd6ea01e4fb618eaeb663c26
Orion Kindel <cakekindel@gmail.com>
2021-02-09 14:38:38 -0700

    wip

--- index.mjs
+++ index.mjs
@@ -3,11 +3,12 @@ import cheerio from 'cheerio';
 
 const purchaseObj = (title, price, inStock) => ({title, price, inStock});
 
-const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021';
+const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021&PageSize=96';
 
 axios.get(url, {responseType: 'text'})
      .then(({data}) => cheerio.load(data))
-     .then($ => $('.items-grid-view').first().text())
+     .then($ => $('.items-grid-view .item-cell')
+                  .map((_, el) => $(el).text())
+                  .get()
+     )
      .then(console.log)
-     
-
