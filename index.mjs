e5bb02731cb838db35ca0bcbe0eeb39ad92ec1a5
Orion Kindel <cakekindel@gmail.com>
2021-02-09 15:20:08 -0700

    stop when we find one

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+import {timer} from 'rxjs';
+import * as RxOp from 'rxjs/operators/index.js';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
@@ -8,16 +10,34 @@ const sendNotification = (data) => {
      console.log(filteredList);
 }
 
+const isEmpty = len => len.length === 0;
+const not = f => (...args) => !f(...args);
+const isAd = $ => (_, el) => $('.txt-ads-box', el).length > 0 ? true : false;
+const parseItem = $ => (_, el) => purchaseObj(
+                                    $(".item-title", el).text(),
+                                    $(".price-current strong", el).text(),
+                                    $("a.item-title", el).attr("href"),
+                                    $(".item-promo", el).text() !== "OUT OF STOCK",
+                                  );
+
 const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021&PageSize=96';
-//TODO traverse pages
-axios.get(url, {responseType: 'text'})
-     .then(({data}) => cheerio.load(data))
-     .then($ => $('.items-grid-view .item-cell')
-                  .map((_, el) => purchaseObj(
-                                   $(".item-title", el).text(),
-                                   $(".price-current strong", el).text(),
-                                   $("a.item-title", el).attr("href"),
-                                   $(".item-promo", el).text() !== "OUT OF STOCK"))
-                  .get()
-     )
-     .then(sendNotification)
+
+const getInStock = () => axios.get(url, {responseType: 'text'})
+                              .then(({data}) => cheerio.load(data))
+                              .then($ => $('.items-grid-view .item-cell')
+                                           .filter(not(isAd($)))
+                                           .map(parseItem($))
+                                           .get()
+                              )
+                              .then(arr => arr.filter(item => item.inStock))
+                              .then(arr => {
+                                console.log(arr);
+                                return arr;
+                              });
+
+timer(0, 500)
+  .pipe(
+    RxOp.flatMap(getInStock),
+    RxOp.takeWhile(isEmpty),
+  )
+  .subscribe();
