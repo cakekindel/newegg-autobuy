36292a60f10f6fc7c7b9c2cc9851d034b15a8288
Steven Baron <Steven.Baron@strongmind.com>
2021-02-09 14:52:37 -0700

    added method to send notif

--- index.mjs
+++ index.mjs
@@ -1,14 +1,23 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-const purchaseObj = (title, price, inStock) => ({title, price, inStock});
+const purchaseObj = (title, price, url, inStock) => ({title, price, url, inStock});
+
+const sendNotification = (data) => {
+     let filteredList = data.filter(e => e.inStock);
+     console.log(filteredList);
+}
 
 const url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=4020%204021&PageSize=96';
-
+//TODO traverse pages
 axios.get(url, {responseType: 'text'})
      .then(({data}) => cheerio.load(data))
      .then($ => $('.items-grid-view .item-cell')
-                  .map((_, el) => $(el).text())
+                  .map((_, el) => purchaseObj(
+                                   $(".item-title", el).text(),
+                                   $(".price-current strong", el).text(),
+                                   $("a.item-title", el).attr("href"),
+                                   $(".item-promo", el).text() !== "OUT OF STOCK"))
                   .get()
      )
-     .then(console.log)
+     .then(sendNotification)
